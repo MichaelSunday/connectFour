d93b1b77f1b0a4ec332ea8fd906283d6ec1cc382
Michael Domingo <MichaelDomingo@Michaels-MacBook-Air.local>
2015-08-26 11:55:39 -0700

    winLogicGood

--- Js/app.js
+++ Js/app.js
@@ -81,7 +81,7 @@ function checkDiagRightDown() {
 
 function checkDiagRightUp() {
 	var sym = getCur();
-	for (var i = 0; i < board.length - 3; i++) {
+	for (var i = 3; i < board.length; i++) {
 		for (var j =0; j < board[i].length - 3; j++) {
 			if ( (board[i][j] == board[i-1][j+1]) &&
 				 (board[i][j] == board[i-2][j+2]) &&
