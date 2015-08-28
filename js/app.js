b185aa89e5b25420e12d4cfc1af942a8f95732e8
Michael Domingo <MichaelDomingo@Michaels-MacBook-Air.local>
2015-08-28 09:17:42 -0700

    comments

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $('.col').click(function() {
 	console.log(checkDiagRightUp());
 
 	});
-
+//6 rows made in array
 var board = [ [0, 0, 0, 0, 0, 0, 0],
 		      [0, 0, 0, 0, 0, 0, 0],
 		      [0, 0, 0, 0, 0, 0, 0],
@@ -20,7 +20,7 @@ var board = [ [0, 0, 0, 0, 0, 0, 0],
 		      [0, 0, 0, 0, 0, 0, 0],
 		      [0, 0, 0, 0, 0, 0, 0]
 		];  
-
+//return player 1 or 2 in board
 function getCur() {	
 
 if (clickCount % 2 === 1) {
@@ -32,7 +32,7 @@ if (clickCount % 2 === 1) {
 	        	return 2;
 	        }
 }
-
+//start at col 4- down all 4 rows. 1+2+3 to get the remaining 3 cols.
 function checkHoriz() {
 	var sym = getCur();
 	for (var i = 0; i < board.length; i++)  { //rows		
@@ -51,7 +51,7 @@ function checkHoriz() {
 	}
 	return false;
 }
-
+//start at row 3. across all cols.
 function checkVert() {
 	var sym = getCur();
 	for (var i = 0; i < board.length - 3; i++) {
@@ -65,7 +65,7 @@ function checkVert() {
 	}
 	return false;
 }
-
+//checks from 0-2 row, col 0-3 which connects only dia.
 function checkDiagRightDown() {
 	var sym = getCur();
 	for (var i = 0; i < board.length - 3; i++) {
@@ -79,7 +79,7 @@ function checkDiagRightDown() {
 	}
 	return false
 }
-
+//starts from rows 3-5, col 0-3 checks only diag going the opposite way of right down
 function checkDiagRightUp() {
 	var sym = getCur();
 	for (var i = 3; i < board.length; i++) {
@@ -101,11 +101,12 @@ var colIndex;
 var boxIndex;
 var clickCount = 0;
 
+//click counter thats drops to the next opening slot. 
 function dropGamePiece(column) {
 var boxes = $(column).children();
 var curBox = boxes.eq(boxIndex);
 	clickCount++;
-	// $(this).css("transform", "rotateY(180dec");
+
 		if (clickCount % 2 === 1) {
 			$(curBox).css('background-color', 'blue');
 			board[boxIndex][colIndex] = 1;
@@ -119,7 +120,7 @@ var curBox = boxes.eq(boxIndex);
 	    console.log('CURRENT BOARD');    
 	    console.log(printBoard());
 };
-
+//checks if their is an opening
 function isOpen(elm) {
 	var color = $(elm).css('background-color')
 
@@ -129,7 +130,7 @@ function isOpen(elm) {
 				return true;
 		}
 }	
-
+//checks all children of each column if their is a next opening in the col
 function getNextOpen(column){
 	 var boxes = $(column).children();
 
